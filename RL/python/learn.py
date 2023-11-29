94a18ab98841ef920e1aa46eaba699853829808d
Mas233 <mas233@icloud.com>
2023-11-29 22:10:11 +0800

    implementing RL

--- RL/python/learn.py
+++ RL/python/learn.py
@@ -18,12 +18,34 @@ import torchvision.transforms as T
 
 env = gym.make('CartPole-v0').unwrapped
 
-# set up matplotlib
+# matplotlib相关设置
 is_ipython = 'inline' in matplotlib.get_backend()
 if is_ipython:
     from IPython import display
 
 plt.ion()
 
-# if gpu is to be used
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# 命名元组，将(状态,动作)映射到其(next_state, reward)结果
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
+
+
+class ReplayMemory(object):
+
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+        self.position = 0
+
+    def push(self, *args):
+        """保存一个Transition"""
+        if len(self.memory) < self.capacity:
+            self.memory.append(None)
+        self.memory[self.position] = Transition(*args)
+        self.position = (self.position + 1) % self.capacity
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
